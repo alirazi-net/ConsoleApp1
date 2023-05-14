3d8db3624989a598eb148e9806e266c2efbb4222
hadiranjbar80 <ranjbarhadi314@gmail.com>
2023-05-14 09:56:47 +0430

    xcvbvcbv

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Razi;
+
 Console.WriteLine("Hello, Razi!");
 Console.WriteLine("hi mohandesa");
 // Add an object to the class
 // ok
 // yay
+
+Class1 class1   = new Class1();
